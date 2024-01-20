6d9c2afffe70d83b96859402b116bbaf65b0d56d
Zequar <martin.galoux@epitech.eu>
2024-01-20 02:07:18 +0100

    Toolkit remade

--- src/pages/projects/TechnoBadges.tsx
+++ src/pages/projects/TechnoBadges.tsx
@@ -1,19 +1,23 @@
 import { motion as m } from 'framer-motion'
-import { IconDefinition, faReact } from '@fortawesome/free-brands-svg-icons'
+import { IconDefinition, faNodeJs } from '@fortawesome/free-brands-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import NextLogo from '../../assets/NextLogo.png'
 import TailwindLogo from '../../assets/TailwindLogo.png'
+import EtherLogo from '../../assets/EtherLogo.png'
+import SolidityLogo from '../../assets/SolidityLogo.png'
 
 const Badge = ({
   text,
   img,
   icon,
   colorGradient,
+  label,
 } : {
   text?: string
   img?: string
   icon?: IconDefinition
   colorGradient: [ string, string ]
+  label?: string
 }) => {
   const gradientColors = colorGradient;
 
@@ -22,7 +26,9 @@ const Badge = ({
   };
 
   return (
-    <div className="rounded-full w-24 h-24 flex items-center justify-center" style={gradientStyle}>
+    <li className='flex flex-col items-center gap-2'>
+      <div className="rounded-full w-24 h-24 flex items-center justify-center" style={gradientStyle}>
+
         <div className="rounded-xl w-14 h-14 flex items-center justify-center bg-black bg-opacity-30">
           {
             text ? 
@@ -34,7 +40,13 @@ const Badge = ({
             : <></>
           }
         </div>
+
       </div>
+        {
+          label && 
+          <p className='text-sm'>{label}</p>
+        }
+      </li>
   )
 }
 
@@ -51,21 +63,29 @@ function TechnoBadges() {
           My Favorite Toolkit
         </m.h2>
       </div>
-      <ul className='flex flex-wrap gap-4 my-8'>
-        <li>
-          <Badge text='JS' colorGradient={['#ffe18f', '#f0db4f']}/>
-        </li>
-        <li>
-          <Badge icon={faReact} colorGradient={[ '#066d89', '#36dff8']}/>
-        </li>
-        <li>
-          <Badge img={NextLogo} colorGradient={['#000000', '#a1a3bb']}/>
-        </li>
-        <li>
-          <Badge img={TailwindLogo} colorGradient={['#24A2B8', '#0ab970']}/>
-        </li>
-
-      </ul>
+      <div className='flex sm:flex-wrap md:flex-no-wrap w-full gap-4'>
+        <div className='bg-black p-6 rounded-2xl max-w-[256px] mt-8'>
+          <p className='text-xl font-bold pb-4 text-center'>Frontend</p>
+          <ul className='flex flex-wrap gap-4 justify-center'>
+            <Badge label='Next.js' img={NextLogo} colorGradient={['#000000', '#a1a3bb']}/>
+            <Badge label='TailwindCSS' img={TailwindLogo} colorGradient={['#24A2B8', '#0ab970']}/>
+          </ul>
+        </div>
+        <div className='bg-black p-6 rounded-2xl max-w-[256px] mt-8'>
+          <p className='text-xl font-bold pb-4 text-center'>Smart Contracts</p>
+          <ul className='flex flex-wrap gap-4 justify-center'>
+            <Badge label='Solidity' img={SolidityLogo} colorGradient={['#000000', '#a1a3bb']}/>
+            <Badge label='Ethereum' img={EtherLogo} colorGradient={['#7487D0', '#9BAADE']}/>
+          </ul>
+        </div>
+        <div className='bg-black p-6 rounded-2xl max-w-[256px] mt-8'>
+          <p className='text-xl font-bold pb-4 text-center'>Backend</p>
+          <ul className='flex flex-wrap gap-4 justify-center'>
+            <Badge label='Node.js' icon={faNodeJs} colorGradient={['#78B360', '#38771c']}/>
+            <Badge label='SQL' text='SQL' colorGradient={[ '#066d89', '#36dff8']}/>
+          </ul>
+        </div>
+      </div>
     </section>
   )
 }
